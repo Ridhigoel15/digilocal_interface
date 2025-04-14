f45197f3028911f5fb17d61d1f2a381afe8b4e20
Ridhigoel15 <ridhiigoel@gmail.com>
2025-04-14 15:17:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Digilocal Interface")
+
+st.title("Digilocal Request Interface")
+st.write("Hello! This will help automate audience extraction for Meta and Google platforms.")
+
